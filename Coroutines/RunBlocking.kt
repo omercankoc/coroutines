64d2733bbe650f1b2aeed848d58a9924da814957
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-16 20:08:27 +0300

    Update RunBlocking.kt

--- Coroutines/RunBlocking.kt
+++ Coroutines/RunBlocking.kt
@@ -2,11 +2,11 @@ import kotlinx.coroutines.*
 
 fun main(){
     println("Run Blocking Start!")
-	  runBlocking {
-  	    launch {
-    		    delay(5000)
-    		    println("Run Blocking!")
-  		  }
-	  }
-	  println("Run Blocking End!")
+    runBlocking {
+  	launch {
+            delay(5000)
+            println("Run Blocking!")
+        }
+    }
+    println("Run Blocking End!")
 }
