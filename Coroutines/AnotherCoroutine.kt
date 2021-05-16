203b6b177ca01124f317aa09b3aa8460363f1ed8
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-16 20:30:06 +0300

    Update AnotherCoroutine.kt

--- Coroutines/AnotherCoroutine.kt
+++ Coroutines/AnotherCoroutine.kt
@@ -4,15 +4,15 @@ fun main(){
     // Baska Coroutines icerisinden Coroutines yurutme: 
     // Once Coroutine Scope daha sonra Run Blocking calisir.
     runBlocking {
-  	  launch {
-    	  delay(5000)
-    		print("Run Blocking")
-  		}
-  		coroutineScope {
-    		launch {
-      			delay(2500)
-      			print("Coroutine Scope")
-    		}
-  		}
-	 }
+        launch {
+    	    delay(5000)
+    	    print("Run Blocking")
+  	}
+  	coroutineScope {
+    	    launch {
+      	        delay(2500)
+      	        print("Coroutine Scope")
+    	    }
+        }
+    }
 }
