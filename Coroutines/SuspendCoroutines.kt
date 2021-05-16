0aa989434d10e28b8f9630ba9cb07acf9c84ae2c
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-16 20:20:07 +0300

    Update SuspendCoroutines.kt

--- Coroutines/SuspendCoroutines.kt
+++ Coroutines/SuspendCoroutines.kt
@@ -1,1 +1,10 @@
+import kotlinx.coroutines.*
 
+fun main(){
+    print("Coroutine Scope Start!")
+	  CoroutineScope(Dispatchers.Default).launch {
+  		delay(5000)
+  		print("Coroutine Scope")
+	}
+	print("Coroutine Scope End!")
+}
