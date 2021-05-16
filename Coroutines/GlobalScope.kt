adc9346ec43b98df3d89eba59a4d9c7b07eb9103
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-16 20:00:11 +0300

    Update GlobalScope.kt

--- Coroutines/GlobalScope.kt
+++ Coroutines/GlobalScope.kt
@@ -1,1 +1,12 @@
+import kotlinx.coroutines.*
 
+fun main(){
+  // Uygulamanin tumunde calisabilecek bir kapsamda yurutulur.
+	GlobalScope.launch {
+  		repeat(10){
+            launch{
+                println("In the Global Scope!")
+            }
+        }
+  	}
+}
